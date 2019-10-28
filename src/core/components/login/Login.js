efce0aca2838309498bc54ecd567c9b2823e8bb0
Kushal Amin <kushalamin0@gmail.com>
2019-10-28 02:12:39 -0400

    setup login page

--- src/core/components/login/Login.js
+++ src/core/components/login/Login.js
@@ -6,6 +6,14 @@ const Login = (props) => {
 
     const [refferRedirect, toggleAuthenication] = useState(false);
     const [cookies, setCookie] = useCookies(['Auth']);
+    const [userDetail, setUserDetail] = useState({
+        userName: '',
+        password: '',
+        error: false
+    });
+
+
+    const { userName, password , error } = userDetail;
 
     useEffect(() => {
         const { history } = props;
@@ -15,20 +23,53 @@ const Login = (props) => {
     })
 
     const onLogin = () => {
-        setCookie('Auth', true)
-        toggleAuthenication(cookies.Auth);
+        if (userName === 'admin' && password === 'admin') {
+            setCookie('Auth', true)
+            toggleAuthenication(cookies.Auth);
+        } else {
+            setUserDetail({
+                userName: '',
+                password: '',
+                error: true
+            })
+        }
     }
 
     return (
         refferRedirect ? <Redirect to='/shipping' />
         :(
-            <div> In Log In
+            <div className="login-container"> 
+                <input
+                    className="form-control"
+                    placeholder="User Name"
+                    type="text"
+                    onChange={(e) => {
+                        setUserDetail({ ...userDetail, userName : e.target.value})
+                    }}
+                    value={userName}
+                    autoFocus
+                />
+                <input
+                    className="form-control"
+                    placeholder="******"
+                    type="password"
+                    onChange={(e) => {
+                        setUserDetail({ ...userDetail, password : e.target.value })
+                    }}
+                    value={password}
+                    autoFocus
+                />
                 <button
                     type="button"
                     onClick={onLogin}
                 >
                     Click to Log In
                 </button>
+                { error && 
+                    <div className='error-container'>
+                        Sorry! not Authorized ...
+                    </div>             
+                }
             </div>
         )
         
